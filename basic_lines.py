8a0533cb612245b70a3d119dd36ed4d39544e94d
Matheus Pedroni <pnmatheus@protonmail.com>
2024-05-01 20:59:05 -0300

    remove perspective correction

--- basic_lines.py
+++ basic_lines.py
@@ -167,113 +167,6 @@ plt.imshow(image_with_contours)
 
 # %%
 
-contours, _ = cv2.findContours(
-    smoothed_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
-)
-
-convex_hull_points = cv2.convexHull(np.concatenate(contours))
-
-image_with_hull = cv2.cvtColor(smoothed_image, cv2.COLOR_GRAY2BGR)
-
-# %%
-
-
-def convex_hull_with_n_points(points, n):
-    # Compute the convex hull of the input points
-    hull = cv2.convexHull(points)
-    full_area = cv2.contourArea(hull)
-
-    # If the number of points in the convex hull is greater than the desired number of points
-    if len(hull) > n:
-        # Initialize the smallest convex hull with all points
-        smallest_hull = hull.copy()
-
-        # Iteratively remove points until the desired number is reached
-        while len(smallest_hull) > n:
-            min_area_diff = float("inf")
-            point_to_remove = None
-
-            # Calculate the area of the convex hull after removing each point
-            for i, point in enumerate(smallest_hull):
-                temp_hull = np.delete(smallest_hull, i, axis=0)
-                area = cv2.contourArea(temp_hull)
-                area_diff = abs(full_area - area)
-                if area_diff < min_area_diff:
-                    min_area_diff = area_diff
-                    point_to_remove = i
-
-            # Remove the point that results in the smallest change in area
-            smallest_hull = np.delete(smallest_hull, point_to_remove, axis=0)
-
-        downsized_hull = smallest_hull
-    else:
-        downsized_hull = hull
-
-    return cv2.convexHull(downsized_hull.astype(np.int32))
-
-
-# https://gist.github.com/flashlib/e8261539915426866ae910d55a3f9959
-def order_points_new(pts):
-    # sort the points based on their x-coordinates
-    xSorted = pts[np.argsort(pts[:, 0]), :]
-
-    # grab the left-most and right-most points from the sorted
-    # x-roodinate points
-    leftMost = xSorted[:2, :]
-    rightMost = xSorted[2:, :]
-
-    # now, sort the left-most coordinates according to their
-    # y-coordinates so we can grab the top-left and bottom-left
-    # points, respectively
-    leftMost = leftMost[np.argsort(leftMost[:, 1]), :]
-    (tl, bl) = leftMost
-
-    # if use Euclidean distance, it will run in error when the object
-    # is trapezoid. So we should use the same simple y-coordinates order method.
-
-    # now, sort the right-most coordinates according to their
-    # y-coordinates so we can grab the top-right and bottom-right
-    # points, respectively
-    rightMost = rightMost[np.argsort(rightMost[:, 1]), :]
-    (tr, br) = rightMost
-
-    # return the coordinates in top-left, top-right,
-    # bottom-right, and bottom-left order
-    return np.array([tl, tr, br, bl], dtype=np.float32)
-
-
-downsized_hull = convex_hull_with_n_points(np.concatenate(contours), 4)
-
-cv2.drawContours(image_with_hull, [convex_hull_points], -1, (0, 255, 0), thickness=10)
-cv2.drawContours(image_with_hull, [downsized_hull], -1, (255, 0, 0), thickness=10)
-
-plt.imshow(image_with_hull)
-
-# %%
-
-height, width = smoothed_image.shape
-hull_corners = order_points_new(downsized_hull.reshape(-1, 2))
-output_corners = np.array(
-    [
-        [2 * PAGE_MARGIN_X, 2 * PAGE_MARGIN_Y],
-        [width - 2 * PAGE_MARGIN_X, 2 * PAGE_MARGIN_Y],
-        [width - 2 * PAGE_MARGIN_X, height - 2 * PAGE_MARGIN_Y],
-        [PAGE_MARGIN_X, height - 2 * PAGE_MARGIN_Y],
-    ],
-    dtype=np.float32,
-)
-output_corners = order_points_new(output_corners)
-
-perspective_matrix = cv2.getPerspectiveTransform(hull_corners, output_corners)
-
-perspective_image = cv2.warpPerspective(image, perspective_matrix, (width, height))
-
-plt.imshow(smoothed_image)
-plt.show()
-plt.imshow(perspective_image)
-
-# %%
-
 
 def pix2norm(shape, pts):
     height, width = shape[:2]
@@ -300,40 +193,9 @@ for points in region_points:
     sampled_points = sampled_points.reshape(-1, 1, 2)
     span_points.append(sampled_points)
 
-# %%
-
-# region_points = []
-# span_points = []
-# for y_avg in y_values:
-#     valid_indices = np.where(y_avg > 0)[0]
-#     points = np.array([(index, y_avg[index]) for index in valid_indices])
-#     x = points[:, 0]
-#     y = points[:, 1]
-
-#     coefficients = np.polyfit(x, y, 3)
-#     poly_function = np.poly1d(coefficients)
-
-#     sampled_x = np.linspace(min(x), max(x), 15)  # Adjust the number of points as needed
-#     sampled_y = poly_function(sampled_x)
-
-#     sampled_points = np.hstack([sampled_x.reshape(-1, 1), sampled_y.reshape(-1, 1)])
-#     region_points.append(sampled_points)
-#     sampled_points = pix2norm(image.shape, sampled_points)
-#     sampled_points = sampled_points.reshape(-1, 1, 2)
-#     span_points.append(sampled_points)
-
-#     # # Plot the original points and the fitted polynomial curve
-#     # plt.scatter(x, y, color='blue', label='Original Points')
-#     # plt.plot(sampled_x, sampled_y, color='red', label='Fitted Polynomial Curve')
-#     # plt.xlabel('X')
-#     # plt.ylabel('Y')
-#     # plt.title('Fitted Polynomial Curve')
-#     # plt.legend()
-#     # plt.grid(True)
-#     # plt.show()
-#     # break
 
 # %%
+
 point_image = rotated_image.copy()
 
 for region in region_points:
@@ -542,7 +404,6 @@ def round_nearest_multiple(i, factor):
     return i + factor - rem if rem else i
 
 
-# TODO continue here
 def remap(img, page_dims, params):
     height = 0.5 * page_dims[1] * OUTPUT_ZOOM * img.shape[0]
     height = round_nearest_multiple(height, REMAP_DECIMATE)
