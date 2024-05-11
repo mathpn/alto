e09a120d9ba764b9f50c02917098d344f9fb6318
Matheus Pedroni <pnmatheus@protonmail.com>
2024-05-11 14:56:12 -0300

    adjust derotate script

--- derotate.py
+++ derotate.py
@@ -4,7 +4,7 @@ import cv2
 import numpy as np
 from scipy.spatial import distance
 
-from dewarp import ADAPTIVE_WINSZ, get_horizontal_lines
+from dewarp import ADAPTIVE_WINSZ, get_horizontal_lines, save_debug_image
 
 
 def rotate_image(image, angle):
@@ -21,42 +21,60 @@ def rotate_image(image, angle):
     return result
 
 
-def derotate_image(image, horizontal_lines, min_relative_width: float):
-    min_line_size = min_relative_width * image.shape[0]
-    angles, sizes = [], []
+def calc_average_angle(image, horizontal_lines, min_relative_width: float, debug: bool):
+    min_line_size = min_relative_width * image.shape[1]
+    angles, sizes, valid_lines = [], [], []
     for line in horizontal_lines:
         x1, y1, x2, y2 = line[0]
         size = distance.euclidean([x1, y1], [x2, y2])
         if size < min_line_size:
             continue
+
+        valid_lines.append(line)
         angle = np.arctan2(y2 - y1, x2 - x1) * 180 / np.pi
         angles.append(angle)
         sizes.append(size)
 
-    average_angle = (np.array(angles) * np.array(sizes)).sum() / np.sum(sizes)
-    print(f"average angle: {average_angle:.2f}")
+    if debug:
+        debug_image = image.copy()
+        debug_image = cv2.cvtColor(debug_image, cv2.COLOR_GRAY2BGR)
+        for line in valid_lines:
+            x1, y1, x2, y2 = line[0]
+            cv2.line(debug_image, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        save_debug_image(debug_image, "02_lines")  # XXX separate debug from dewarp
 
-    rotated_image = rotate_image(image, average_angle)
-    return rotated_image
+    average_angle = (np.array(angles) * np.array(sizes)).sum() / (1e-3 + np.sum(sizes))
+    return average_angle
 
 
 def derotate(input_image: str, max_angle: int, min_relative_width: float, debug: bool):
     image = cv2.imread(input_image)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    horizontal_lines = get_horizontal_lines(gray, max_angle, debug)
-    derotated_img = derotate_image(gray, horizontal_lines, min_relative_width)
+    height, width = gray.shape
+    aspect_ratio = height / width
+    small_width = 1500  # XXX
+    small_height = int(small_width * aspect_ratio)
+    gray_small = cv2.resize(gray, (small_width, small_height))
+
+    horizontal_lines = get_horizontal_lines(gray_small, max_angle, debug)
+    average_angle = calc_average_angle(
+        gray_small, horizontal_lines, min_relative_width, debug
+    )
+    print(f"average angle: {average_angle:.2f}")
+    derotated_img = rotate_image(gray, average_angle)
     return derotated_img
 
 
 def main():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
     parser.add_argument("--input-image", type=str, required=True)
-    parser.add_argument("--debug", action="store_true")
     parser.add_argument(
         "--max-line-angle",
         type=int,
-        default=30,
+        default=15,
         help="Maximum allowed angle for approximately horizontal lines",
     )
     parser.add_argument(
@@ -68,9 +86,10 @@ def main():
     parser.add_argument(
         "--min-relative-width",
         type=float,
-        default=0.2,
+        default=0.075,
         help="Minimum width relative to maximum width for a line to be considered",
     )
+    parser.add_argument("--debug", action="store_true")
     args = parser.parse_args()
 
     derotated_img = derotate(
