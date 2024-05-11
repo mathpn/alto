2b867ffeeb7d2101984997e5531af308a1168bcd
Matheus Pedroni <pnmatheus@protonmail.com>
2024-05-11 14:56:29 -0300

    draft of derotate_pdf script

---
+++ derotate_pdf.py
@@ -0,0 +1,51 @@
+import argparse
+import os
+
+import cv2
+
+from derotate import ADAPTIVE_WINSZ, derotate
+from extract_images import extract_pdf_images
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument("--pdf-file", type=str, required=True)
+    parser.add_argument("--img-output-dir", type=str, default="./images")
+    parser.add_argument(
+        "--max-line-angle",
+        type=int,
+        default=30,
+        help="Maximum allowed angle for approximately horizontal lines",
+    )
+    parser.add_argument(
+        "--adaptive-winsz",
+        type=int,
+        default=ADAPTIVE_WINSZ,
+        help="Window size for adaptive threshold in reduced px",
+    )
+    parser.add_argument(
+        "--min-relative-width",
+        type=float,
+        default=0.2,
+        help="Minimum width relative to maximum width for a line to be considered",
+    )
+    parser.add_argument("--debug", action="store_true")
+    args = parser.parse_args()
+
+    img_files = extract_pdf_images(args.pdf_file, args.img_output_dir)
+    print(img_files)
+    for i, img_file in enumerate(img_files):
+        print(f"-> dewarping image {i+1}/{len(img_files)}")
+        img_file_path = os.path.join(args.img_output_dir, img_file)
+        derotated_img = derotate(
+            img_file_path, args.max_line_angle, args.min_relative_width, args.debug
+        )
+        out_img_file = "{0}_{2}{1}".format(*os.path.splitext(img_file) + ("derotate",))
+        out_img_file_path = os.path.join(args.img_output_dir, out_img_file)
+        cv2.imwrite(out_img_file_path, derotated_img)
+
+
+if __name__ == "__main__":
+    main()
