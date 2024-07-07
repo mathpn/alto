fecfc1f5a578ae2de3f16ba31caa3d27864e7fbc
Matheus Pedroni <pnmatheus@protonmail.com>
2024-07-06 21:34:20 -0300

    also pad image in dewarp PDF script

--- dewarp_pdf.py
+++ dewarp_pdf.py
@@ -6,7 +6,7 @@ import cv2
 from fpdf import FPDF, Align
 
 from dewarp import Config, dewarp
-from extract_images import extract_pdf_images
+from extract_images import add_borders_to_aspect_ratio, extract_pdf_images
 
 
 def main():
@@ -29,19 +29,21 @@ def main():
 
     img_files = extract_pdf_images(args.pdf_file, args.img_output_dir)
     pdf = FPDF(format=args.page_format)
+    pdf.set_margins(0, 0, 0)
     for i, img_file in enumerate(img_files):
         print(f"-> dewarping image {i+1}/{len(img_files)}")
         img_file_path = os.path.join(args.img_output_dir, img_file)
         dewarped_img = dewarp(img_file_path, config, args.debug)
+        dewarped_img = add_borders_to_aspect_ratio(dewarped_img, pdf.epw / pdf.eph)
         out_img_file = "{0}_{2}{1}".format(*os.path.splitext(img_file) + ("dewarp",))
         out_img_file_path = os.path.join(args.img_output_dir, out_img_file)
         cv2.imwrite(out_img_file_path, dewarped_img)
         pdf.add_page()
-        pdf.image(out_img_file_path, 0, 0, pdf.epw, pdf.eph, keep_aspect_ratio=True)
+        pdf.image(out_img_file_path, Align.C, None, pdf.epw, pdf.eph)
 
     pdf_path = f"{Path(args.pdf_file).stem}_dewarped.pdf"
     pdf.output(pdf_path)
-    print(f"saved derotated PDF to {pdf_path}")
+    print(f"saved dewarped PDF to {pdf_path}")
 
 
 if __name__ == "__main__":
